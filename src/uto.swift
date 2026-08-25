bfc91ca7e8cc9ca6ba7c01fd6546cf3017505259
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:27:55 +0300

    Add HTTPMethod enum for request methods

--- src/uto.swift
+++ src/uto.swift
@@ -20,6 +20,13 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
 
 public class Uto {
     private let api = "https://u.to/api"
